0ed92b5372face3db47b227c3771e0f2674e04c7
raeperd <raeperd117@gmail.com>
2023-07-01 18:08:51 +0900

    feat: Add automcmd YankHighlight

--- init.lua
+++ init.lua
@@ -23,3 +23,15 @@ vim.opt.scrolloff = 8
 
 vim.opt.splitright = true
 vim.opt.splitbelow = true
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
