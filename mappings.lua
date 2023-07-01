c077cf13b0ab398f73e3ac0bf22f2af624d653a1
raeperd <raeperd117@gmail.com>
2023-07-01 16:36:03 +0900

    feat(nvim-tmux-navigator): Add tmux configuration

--- mappings.lua
+++ mappings.lua
@@ -8,12 +8,27 @@ M.general = {
 }
 
 -- more keybinds!
+-- https://nvchad.com/docs/config/mappings
 M.nvimtree = {
 	plugin = true,
 	n = {
 		["<leader>ex"] = { "<cmd> NvimTreeToggle <CR>", "Toggle nvimtree" },
 		["<leader>ef"] = { "<cmd> NvimTreeFocus <CR>", "Focus nvimtree" },
 	},
+}
+
+local tmuxKeyMaps = {
+  ["<C-h>"] = {"<cmd> NvimTmuxNavigateLeft <CR>", "NvimTmuxNavigateLeft"},
+  ["<C-j>"] = {"<cmd> NvimTmuxNavigateDown <CR>", "NvimTmuxNavigateDown"},
+  ["<C-k>"] = {"<cmd> NvimTmuxNavigateUp <CR>", "NvimTmuxNavigateUp"},
+  ["<C-l>"] = {"<cmd> NvimTmuxNavigateRight <CR>", "NvimTmuxNavigateRight"},
+  ["<C-\\>"] = {"<cmd> NvimTmuxNavigateLastActive <CR>", "NvimTmuxNavigateLastActive"},
+  ["<C-Space>"] = {"<cmd> NvimTmuxNavigateNext <CR>", "NvimTmuxNavigateNext"},
+}
+M.tmux = {
+	n = tmuxKeyMaps,
+	i = tmuxKeyMaps,
+	v = tmuxKeyMaps,
 }
 
 return M
