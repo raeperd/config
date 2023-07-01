591f584631f92d030b11594bd8bdee16bd775509
raeperd <raeperd117@gmail.com>
2023-07-01 16:59:20 +0900

    feat: Add vim options

--- init.lua
+++ init.lua
@@ -5,3 +5,21 @@
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+
+vim.wo.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
