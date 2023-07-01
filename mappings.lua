c2853a9cd57f43a0266548fb6671c125dc098704
raeperd <raeperd117@gmail.com>
2023-07-01 16:54:39 +0900

    feat(telescope): Remap keybindings

--- mappings.lua
+++ mappings.lua
@@ -17,6 +17,28 @@ M.nvimtree = {
 	},
 }
 
+M.telescope = {
+  plugin = true,
+
+  n = {
+    -- find
+    ["<leader>ff"] = { "<cmd> Telescope find_files follow=true <CR>", "Find files" },
+    ["<leader>fp"] = { "<cmd> Telescope live_grep <CR>", "Find by Grep" },
+    ["<leader>fb"] = { "<cmd> Telescope buffers <CR>", "Find buffers" },
+    ["<leader>fh"] = { "<cmd> Telescope help_tags <CR>", "Help page" },
+    ["<leader>fc"] = { "<cmd> Telescope current_buffer_fuzzy_find <CR>", "Find in current buffer" },
+    ["<leader>fk"] = { "<cmd> Telescope keymaps <CR>", "Find in current buffer" },
+
+    -- git
+    ["<leader>fgf"] = { "<cmd> Telescope git_files <CR>", "Git commits" },
+    ["<leader>fgc"] = { "<cmd> Telescope git_commits <CR>", "Git commits" },
+    ["<leader>fgs"] = { "<cmd> Telescope git_status <CR>", "Git status" },
+
+    -- theme switcher
+    ["<leader>ft"] = { "<cmd> Telescope themes <CR>", "Nvchad themes" },
+  },
+}
+
 local tmuxKeyMaps = {
   ["<C-h>"] = {"<cmd> NvimTmuxNavigateLeft <CR>", "NvimTmuxNavigateLeft"},
   ["<C-j>"] = {"<cmd> NvimTmuxNavigateDown <CR>", "NvimTmuxNavigateDown"},
