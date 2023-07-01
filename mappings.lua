79caf8365fd4e1377daf7210fd356d32975bd7d2
raeperd <raeperd117@gmail.com>
2023-07-01 13:38:11 +0900

    feat(nvim-tree): Fix keymap for toggle tree

--- mappings.lua
+++ mappings.lua
@@ -2,11 +2,18 @@
 local M = {}
 
 M.general = {
-  n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
-  },
+	n = {
+		[";"] = { ":", "enter command mode", opts = { nowait = true } },
+	},
 }
 
 -- more keybinds!
+M.nvimtree = {
+	plugin = true,
+	n = {
+		["<leader>ex"] = { "<cmd> NvimTreeToggle <CR>", "Toggle nvimtree" },
+		["<leader>ef"] = { "<cmd> NvimTreeFocus <CR>", "Focus nvimtree" },
+	},
+}
 
 return M
