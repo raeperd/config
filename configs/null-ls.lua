a8a6f19d223f37e0b83f48789f43a9461e29cb1a
raeperd <raeperd117@gmail.com>
2023-07-02 09:34:53 +0900

    feat(null-ls): Implements gofmt, goimports configuration

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -17,9 +17,29 @@ local sources = {
 
   -- cpp
   b.formatting.clang_format,
+
+  -- go 
+  b.formatting.gofmt,
+  b.formatting.goimports,
 }
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup {
   debug = true,
   sources = sources,
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({
+        group = augroup,
+        buffer = bufnr,
+      })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end
 }
