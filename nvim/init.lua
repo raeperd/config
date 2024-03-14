b27742115f2ccdb82f21bcca026b73cb5f8c944f
raeperd <raeperd117@gmail.com>
2024-03-15 00:44:29 +0900

    feat(nvim): Add golang supports

--- nvim/init.lua
+++ nvim/init.lua
@@ -537,7 +537,8 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
+        golangci_lint_ls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -606,6 +607,7 @@ require('lazy').setup({
       },
       formatters_by_ft = {
         lua = { 'stylua' },
+        go = { 'goimports', 'gofmt' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
